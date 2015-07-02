a23899eb1e617294c7d1faa9fd19033a4a45a1f3
Michael Köppl <mko.mko31@gmail.com>
2015-07-02 10:20:43 +0200

    Update http server to use Gorilla Mux

--- http/http.go
+++ http/http.go
@@ -8,11 +8,37 @@ package http
 
 import (
   "net/http"
+  "github.com/gorilla/mux"
   "github.com/fatih/color"
+  "github.com/codegangsta/negroni"
 )
 
+/*
+ * initialise http server
+ */
 func Init(){
+  r := mux.NewRouter().StrictSlash(false)
+
+  /*
+   * main page
+   */
+  r.HandleFunc("/", mainPageHandler)
+
+  /*
+   * custom profile url
+   */
+  r.HandleFunc("/people/{name}", peoplePageHandler)
+
+  /*
+   * init negroni middleware
+   */
+  n := negroni.New(
+    negroni.NewRecovery(),
+    negroni.HandlerFunc(MiddleWare),
+    //negroni.NewLogger(),
+    negroni.NewStatic(http.Dir("public")),
+  )
+  n.UseHandler(r)
   color.Green("IMGCAT Server running on port 8000")
-	http.HandleFunc("/", mainPageHandler)
-	http.ListenAndServe(":8000", nil)
+  n.Run(":8000")
 }
--- http/reqh.go
+++ http/reqh.go
@@ -1,15 +1,69 @@
 package http
 
 import (
+  "html/template"
+  "os"
   "net/http"
+  "path"
   "github.com/fatih/color"
+  "github.com/gorilla/mux"
 )
 
+func MiddleWare(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc){
+  if true{
+    next(rw, r)
+  }
+}
+
 func mainPageHandler(w http.ResponseWriter, req *http.Request) {
-	resourcePath := "public/html" + req.URL.Path + ".html"
+  color.Cyan("REQ: request for %s.html", req.URL.Path)
+	resourcePath := "public" + req.URL.Path + ".html"
 	if req.URL.Path == "/" {
-		resourcePath = "public/html/" + "welcome.html"
+	   resourcePath = "public/" + "welcome.html"
 	}
-	color.Red("serving static file => %s", resourcePath)
-	http.ServeFile(w, req, resourcePath)
+
+  if _, err := os.Stat(resourcePath); os.IsNotExist(err) {
+     color.Red("ERR: 404. no such file or directory: %s.html", req.URL.Path)
+     http.Error(w, ("error 404. " + req.URL.Path + ".html could not be found."), 404)
+  }else{
+	   color.Green("INF: serving static file => %s", resourcePath)
+	   http.ServeFile(w, req, resourcePath)
+  }
+}
+
+type User struct{
+  Uname string
+}
+
+func mePageHandler(w http.ResponseWriter, req *http.Request) {
+    /*t := template.New("fieldname example")
+    t, _ = t.ParseFiles("public/html/people.html")
+    p := Person{Uname: "Your"}
+    t.Execute(w, p)*/
+}
+
+func peoplePageHandler(w http.ResponseWriter, req *http.Request){
+  // get variables (name in people/{name}) from request
+  vars := mux.Vars(req)
+
+  // get name
+  name := vars["name"]
+
+  // insert name into User object
+  user := User{name}
+
+  fp := path.Join("public", "people.html")
+
+  // form template
+  tmpl, err := template.ParseFiles(fp)
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  // return the template or print an error if one occurs
+  if err := tmpl.Execute(w, user); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
