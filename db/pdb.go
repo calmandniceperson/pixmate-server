084a0556371530140cb217da257a248535fc468e
Michael Köppl <mko.mko31@gmail.com>
2015-07-05 20:42:32 +0200

    Enable server to get input on db data or use environment variables

--- db/pdb.go
+++ db/pdb.go
@@ -1,14 +1,46 @@
 package db
 
 import (
-  "log"
+  "os"
+  "bufio"
   "github.com/fatih/color"
   "database/sql"
 	_ "github.com/lib/pq"
 )
 
 func Init(){
-  db, err := sql.Open("postgres", "user=imgturtlea password=imgtadmin dbname=imgturtle sslmode=disable")
+  /*
+   * allow user to enter db data
+   * used instead of environment variables
+   * since the service is open source
+   */
+  var (
+    uname string
+    pw string
+    name string
+  )
+  if os.Getenv("DB_UNAME") == "" && os.Getenv("DB_PW") == "" && os.Getenv("DB_NAME") == ""{
+    reader := bufio.NewReader(os.Stdin)
+    color.Cyan("Enter db user name: ")
+    uname, _ = reader.ReadString('\n')
+
+    color.Cyan("Enter db pw: ")
+    pw, _ = reader.ReadString('\n')
+
+    color.Cyan("Enter db name: ")
+    name, _ = reader.ReadString('\n')
+  }else{
+    uname = os.Getenv("DB_UNAME")
+    pw = os.Getenv("DB_PW")
+    name = os.Getenv("DB_NAME")
+  }
+
+  db, err := sql.Open("postgres",
+    "user=" + uname +
+    " password=" + pw +
+    " dbname=" + name +
+    " sslmode=disable")
+
   if err != nil {
     color.Red("ERR: pdb.go Init() => PostgreSQL config could not be established.")
     color.Red(err.Error())
@@ -64,6 +96,6 @@ func Init(){
       color.Red("ERR: pdb.go Init() => An error occured.")
       color.Red(err.Error())
     }
-    */
   }
+  */
 }
