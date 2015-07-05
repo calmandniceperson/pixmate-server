19f86188169497f7dcd5735ed9105e0998b86d1a
Michael Köppl <mko.mko31@gmail.com>
2015-07-05 20:44:41 +0200

    Clear console after running db Init()

--- main.go
+++ main.go
@@ -9,9 +9,24 @@ package main
 import (
 	"imgturtle/http"
 	"imgturtle/db"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
 	db.Init()
+
+	if runtime.GOOS == "windows"{
+		cmd := exec.Command("cls")
+		cmd.Stdout = os.Stdout
+	  cmd.Run()
+	}else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+	  cmd.Run()
+	}
+
+
 	http.Init()
 }
