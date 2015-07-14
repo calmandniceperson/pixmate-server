c7d0ee2fc3a66e7641dec9ce82ccb219a6ce8b6a
Michael Köppl <mko.mko31@gmail.com>
2015-07-14 16:28:20 +0200

    Update tables to be better-fitting
    * All varchar(n) are exchanged for unlimited text
    * DROP TABLES are added before creating new tables
    * Image IDs are now random MD5 hash values
    * User IDs are now random MD5 hash values
    * Image upload dates are now the current time by default
    * User join dates are now the current time by default

--- db/db.sql
+++ db/db.sql
@@ -1,30 +1,33 @@
 /* CREATE SCHEMA imgturtle FOR USER/ROLE imgturtle */
 CREATE SCHEMA IF NOT EXISTS imgturtle AUTHORIZATION imgturtle;
 
+DROP TABLE IF EXISTS imgturtle.User;
 CREATE TABLE IF NOT EXISTS imgturtle.User (
-    user_id bigserial not null,
-    user_name varchar(40) not null,
-    user_pw varchar(50) not null,
+    user_id text default md5(random()::text),
+    user_name text not null,
+    user_pw text not null,
     date_joined timestamp default null,
     PRIMARY KEY(user_id, user_name)
 );
 
+DROP TABLE IF EXISTS imgturtle.Img;
 CREATE TABLE IF NOT EXISTS imgturtle.Img (
-  image_id varchar(100) primary key,
-  image_title varchar(100) not null,
-  image_path varchar(100) not null,
-  image_desc varchar(400),
-  date_uploaded timestamp not null,
-  uploader_id bigserial not null,
-  uploader_name varchar(40) not null,
+  image_id text primary key default md5(random()::text),
+  image_title text not null,
+  image_path text not null,
+  image_desc text,
+  date_uploaded timestamp not null default now(),
+  uploader_id text not null,
+  uploader_name text not null,
   FOREIGN KEY (uploader_id, uploader_name) REFERENCES imgturtle.User(user_id, user_name)
 );
 
+DROP TABLE IF EXISTS imgturtle.Comment;
 CREATE TABLE IF NOT EXISTS imgturtle.Comment (
   comment_id bigserial primary key,
-  image_id varchar(100) REFERENCES imgturtle.Img(image_id),
-  comment_text varchar(100) not null,
-  creator_id bigserial not null,
-  creator_name varchar(40) not null,
+  image_id text REFERENCES imgturtle.Img(image_id),
+  comment_text text not null,
+  creator_id text not null,
+  creator_name text not null,
   FOREIGN KEY (creator_id, creator_name) REFERENCES imgturtle.User(user_id, user_name)
 );
