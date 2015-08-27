408e15bf4be1289bbdc91e9c3b3ef43b9045838c
Michael Köppl <mko.mko31@gmail.com>
2015-08-27 10:32:41 +0200

    Rename setup.sh to go_setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-echo "welcome to imgturtle setup!"
-cd ..
-go get github.com/gorilla/mux
-go get github.com/fatih/color
-go get github.com/codegangsta/negroni
-go get github.com/lib/pq
-go get golang.org/x/crypto/pbkdf2
-go get github.com/asaskevich/govalidator
