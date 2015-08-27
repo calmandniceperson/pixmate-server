4679532d97bb5f8451317e3ad0b5abdd8b175d14
Michael Köppl <mko.mko31@gmail.com>
2015-08-27 10:31:30 +0200

    Update shell scripts

---
+++ go_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "welcome to imgturtle setup!"
+cd ..
+go get github.com/gorilla/mux
+go get github.com/fatih/color
+go get github.com/codegangsta/negroni
+go get github.com/lib/pq
+go get golang.org/x/crypto/pbkdf2
+go get github.com/asaskevich/govalidator
