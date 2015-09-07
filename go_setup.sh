77bcb27aea8fe901083471f0876f664b17983fb9
Michael Köppl <mko.mko31@gmail.com>
2015-09-07 17:24:48 +0200

    Add gorilla sessions import

--- go_setup.sh
+++ go_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "welcome to imgturtle setup!"
+echo "Welcome to imgturtle setup!"
 cd ..
 go get github.com/gorilla/mux
+go get github.com/gorilla/sessions
 go get github.com/fatih/color
 go get github.com/codegangsta/negroni
 go get github.com/lib/pq
 go get golang.org/x/crypto/pbkdf2
 go get github.com/asaskevich/govalidator
+echo "Finished!"
