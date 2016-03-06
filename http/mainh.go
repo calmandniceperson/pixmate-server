2c79357c742815f0386bf707cfb1dbb715b588eb
Michael Köppl <mko.mko31@gmail.com>
2016-03-06 20:50:41 +0100

    Redirect to error page instead of serving it (test)

--- http/mainh.go
+++ http/mainh.go
@@ -14,7 +14,7 @@ func MiddleWare(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) 
 
 func errorHandler(w http.ResponseWriter, req *http.Request) {
 	cio.PrintMessage(1, req.URL.Path+" not found. Serving error.html")
-	http.ServeFile(w, req, "public/error.html")
+	http.Redirect(w, req, "/error", 403)
 }
 
 func mainPageHandler(w http.ResponseWriter, req *http.Request) {
